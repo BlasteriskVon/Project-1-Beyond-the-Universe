579d36d339a1821fbaa88e3a52926d8b0baf727d
J <j.mey.r8@gmail.com>
2019-06-09 01:01:18 -0400

    added firebase database

---
+++ javascript/app.js
@@ -0,0 +1,88 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCHEAOhLzwmgYdqvuIS9yHJwEbIt9BHjW8",
+    authDomain: "project-1-9f8f4.firebaseapp.com",
+    databaseURL: "https://project-1-9f8f4.firebaseio.com",
+    projectId: "project-1-9f8f4",
+    storageBucket: "project-1-9f8f4.appspot.com",
+    messagingSenderId: "19657218555",
+    appId: "1:19657218555:web:efebe7e27b1fe42f"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+ 
+  // button for liquor search
+  $("#submit-btn").on("click", function(event){
+
+    // user's search
+    var searchDrink = $("#search-input").val().trim();
+
+    // "temporary" object for holding drink data
+    var newDrink = {
+        drink: searchDrink,
+    };
+
+    // pushes drink data to the database
+    database.ref().push(newDrink);
+
+    // console logs the temporary variable
+    console.log(newDrink.drink);
+
+    // clears the search box
+    $("#search-input").val("");
+    
+  })
+
+  $("#submit-btn-two").on("click", function(event){
+
+    // user's search
+    var searchIngredient = $("#search-input-two").val().trim();
+
+    // "temporary" object for holding drink data
+    var newDrink = {
+        ingredient: searchIngredient,
+    };
+
+    // pushes drink data to the database
+    database.ref().push(newDrink);
+
+    // console logs the temporary variable
+    console.log(newDrink.ingredient);
+
+    // clears the search box
+    $("#search-input-two").val("");
+    
+  });
+
+  // creates firebase event for adding drinks and ingredients  
+    
+    database.ref().on("child_added", function(childSnapshot){
+
+      var searchDrink = childSnapshot.val().drink;
+  
+      // creates new row of past searched drinks & ingredients
+      var newRow = $("<tr>").text(searchDrink);
+  
+      // appends the new row to the table/html page
+      $("#drink-table > tbody").prepend(newRow);
+  
+    });
+
+    database.ref().on("child_added", function(childSnapshot){
+
+      var searchIngredient = childSnapshot.val().ingredient;
+      
+      // creates new row of past searched drinks & ingredients
+      var newRow = $("<tr>").text(searchIngredient);
+      
+      // appends the new row to the table/html page
+      $("#liquor-table > tbody").prepend(newRow);
+    });
+
+      
+
+    
